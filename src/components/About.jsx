c860836d6aa85d7c18eb08e372b8f2c649313ce0
marvelous <donatusmarvelous09@gmail.com>
2025-01-01 06:28:09 -0800

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,9 +28,9 @@ function About() {
   return (
     <section id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+      <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
 
 
 
